f27135a0095c15f40b8b2f0464f9f1fa281a47a2
UdeeshaPrabhashana <udeeshaprabhashana123@gmail.com>
2024-02-07 20:02:21 +0530

    Removed

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ const connectDB = require("./connectMongo");
 
 connectDB();
 
-// const BookModel = require("./models/book.model");
-// const Team = require("./models/TeamRegistration");
-// const redis = require('./redis')
-
 import Team from "./models/TeamRegistration";
 const ORIGIN = process.env.ORIGIN;
 
@@ -24,15 +20,6 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
-
-
-// const deleteKeys = async (pattern) => {
-//   const keys = await redis.keys(`${pattern}::*`)
-//   console.log(keys)
-//   if (keys.length > 0) {
-//     redis.del(keys)
-//   }
-// }
 
 app.get("/", (req, res) => {
   res.send("Hackaholics API is running");
@@ -73,52 +60,8 @@ app.post("/api/teamregi/register", async (req, res) => {
   }
 });
 
-// app.get("/api/v1/books", async (req, res) => {
-//   const { limit = 5, orderBy = "name", sortBy = "asc", keyword } = req.query;
-//   let page = +req.query?.page;
 
-//   if (!page || page <= 0) page = 1;
-
-//   const skip = (page - 1) * + limit;
-
-//   const query = {};
-
-//   if (keyword) query.name = { $regex: keyword, $options: "i" };
-
-//   const key = `Book::${JSON.stringify({query, page, limit, orderBy, sortBy})}`
-//   let response = null
-//   try {
-//     const cache = await redis.get(key)
-//     if (cache) {
-//       response = JSON.parse(cache)
-//     } else {
-//       const data = await BookModel.find(query)
-//       .skip(skip)
-//       .limit(limit)
-//       .sort({ [orderBy]: sortBy });
-//       const totalItems = await BookModel.countDocuments(query);
-
-//       response = {
-//         msg: "Ok",
-//         data,
-//         totalItems,
-//         totalPages: Math.ceil(totalItems / limit),
-//         limit: +limit,
-//         currentPage: page,
-//       }
-
-//       redis.setex(key, 600, JSON.stringify(response))
-//     }
-    
-//     return res.status(200).json(response);
-//   } catch (error) {
-//     return res.status(500).json({
-//       msg: error.message,
-//     });
-//   }
-// });
-
-// app.get("/api/v1/books/:id", async (req, res) => {
+// app.get("", async (req, res) => {
 //   try {
 //     const data = await BookModel.findById(req.params.id);
 
@@ -139,68 +82,6 @@ app.post("/api/teamregi/register", async (req, res) => {
 //   }
 // });
 
-// app.post("/api/v1/books", async (req, res) => {
-//   try {
-//     const { name, author, price, description } = req.body;
-//     const book = new BookModel({
-//       name,
-//       author,
-//       price,
-//       description,
-//     });
-//     const data = await book.save();
-//     deleteKeys('Book')
-//     return res.status(200).json({
-//       msg: "Ok",
-//       data,
-//     });
-//   } catch (error) {
-//     return res.status(500).json({
-//       msg: error.message,
-//     });
-//   }
-// });
-
-// app.put("/api/v1/books/:id", async (req, res) => {
-//   try {
-//     const { name, author, price, description } = req.body;
-//     const { id } = req.params;
-
-//     const data = await BookModel.findByIdAndUpdate(
-//       id,
-//       {
-//         name,
-//         author,
-//         price,
-//         description,
-//       },
-//       { new: true }
-//     );
-//     deleteKeys('Book')
-//     return res.status(200).json({
-//       msg: "Ok",
-//       data,
-//     });
-//   } catch (error) {
-//     return res.status(500).json({
-//       msg: error.message,
-//     });
-//   }
-// });
-
-// app.delete("/api/v1/books/:id", async (req, res) => {
-//   try {
-//     await BookModel.findByIdAndDelete(req.params.id);
-//     deleteKeys('Book')
-//     return res.status(200).json({
-//       msg: "Ok",
-//     });
-//   } catch (error) {
-//     return res.status(500).json({
-//       msg: error.message,
-//     });
-//   }
-// });
 
 const PORT = process.env.PORT;
 
